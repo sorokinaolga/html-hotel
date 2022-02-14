17130107443c323c93eec4828cb4f53484f12d57
sorokinaolga <sorokinaop@list.ru>
2022-02-14 16:55:48 +0300

    Обновление сборки проекта

--- webpack.config.js
+++ webpack.config.js
@@ -52,12 +52,7 @@ const config = {
       {
         test: /\.(sa|sc|c)ss$/,
         use: [
-          {
-            loader: MiniCssExtractPlugin.loader,
-            options: {
-              hmr: process.env.NODE_ENV === 'development',
-            },
-          },
+          MiniCssExtractPlugin.loader,
           'css-loader',
           'postcss-loader',
           {
