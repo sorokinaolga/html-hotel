572296a95cbe54f3da5e09f0be13433297a6f5f8
sorokinaolga <sorokinaop@list.ru>
2022-02-20 16:10:06 +0300

    Добавлен компонент label

--- src/pages/index/index.js
+++ src/pages/index/index.js
@@ -3,3 +3,4 @@ import '../../styles/index.scss';
 import '../../components/input/input.scss';
 import '../../components/checkbox/checkbox.scss';
 import '../../components/radio-button/radio-button.scss';
+import '../../components/label/label.scss';
