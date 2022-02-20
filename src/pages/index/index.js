3cf1d1b8677e9f0de646803e8cb7c4bcc185a90d
sorokinaolga <sorokinaop@list.ru>
2022-02-20 15:10:19 +0300

    Добавлен компонент radio-button

--- src/pages/index/index.js
+++ src/pages/index/index.js
@@ -2,3 +2,4 @@ import '../../styles/index.scss';
 
 import '../../components/input/input.scss';
 import '../../components/checkbox/checkbox.scss';
+import '../../components/radio-button/radio-button.scss';
